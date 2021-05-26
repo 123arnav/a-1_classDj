ddb29a3a2fbc69d2b2b59e42085849d5dd5a473f
123arnav <74090758+123arnav@users.noreply.github.com>
2021-05-26 15:36:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 
 song="";
-
+leftWristY="";
+leftWristX="";
+rightWristY="";
+rightWristX="";
+Lscore="";
+Rscore="";
 function preload(){
 song=loadSound("music.mp3")
 };
@@ -11,14 +16,33 @@ canvas.position(500,270)
 video=createCapture(VIDEO);
 video.hide();
 posenet=ml5.poseNet(video,modelLoaded);
-
-
+console.log(ml5.version);
+posenet.on("pose", getposes);
 };
 
 function modelLoaded(){
 console.log("loaded")
 };
 
+function getposes(result){
+    if(result.length>0){
+        console.log(result);
+        leftWristY=result[0].pose.leftWrist.y;
+        leftWristX=result[0].pose.leftWrist.x;
+        console.log(leftWristY);
+        console.log(leftWristX);
+        rightWristY=result[0].pose.rightWrist.y;
+        rightWristX=result[0].pose.rightWrist.x;
+        console.log(rightWristY);
+        console.log(rightWristX);
+        Lscore=result[0].pose.keypoints[9].score;
+        Rscore=result[0].pose.keypoints[10].score;
+        console.log(Lscore);
+        console.log(Rscore);
+        //score
+    }
+    //right
+}
 function play(){
     song.setVolume(1);
     song.rate(1.5);
