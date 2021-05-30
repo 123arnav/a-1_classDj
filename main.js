d51666d0270cf1131a064bea3cf14602981780fd
123arnav <74090758+123arnav@users.noreply.github.com>
2021-05-30 11:58:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ rightWristY="";
 rightWristX="";
 Lscore="";
 Rscore="";
+status="";
 function preload(){
 song=loadSound("music.mp3")
 };
@@ -40,17 +41,27 @@ function getposes(result){
         console.log(Lscore);
         console.log(Rscore);
         
-        //score
+
     }
-    //right
+    
 }
-function play(){
-    song.setVolume(1);
-    song.rate(1.5);
-song.play();
-};
+
+
+
+
 function stop(){
-    song.stop();
+    status=song.isPlaying();
+    console.log(status);
+    if (status=="false"){
+        song.setVolume(1);
+        song.rate(1.5);
+    song.play();
+    document.getElementById("b1").innerHTML="stop";
+    }
+    else if(status=="true"){
+        song.stop();
+        document.getElementById("b1").innerHTML="play";
+    }
     };
 
 function draw(){
@@ -61,12 +72,39 @@ if (Lscore>0.2){
     volumeVal=Number(leftWristY);
     volumeVal=floor(volumeVal);
     volumeVal=volumeVal/500;
-    console.log(volumeVal);
+
     song.setVolume(volumeVal);
     document.getElementById("volume_value").innerHTML=volumeVal;
     document.getElementById("instruct").innerHTML="NOTE:Move your left-hand wrist up and down for increasing and decreasing the volume move your right-hand wrist up and down for increasing and decreasing the speed";
 }
 if(Lscore<0.2){
+    document.getElementById("instruct").innerHTML="please keep a two feet distance and show your wrists to the camera";
+};
+if (Rscore>0.2){
+    fill("purple")
+    circle(rightWristX,rightWristY,50);
+    if(rightWristY>0 && rightWristY<=100){
+        song.rate(0.5);
+        document.getElementById("speed_val").innerHTML="0.5x";
+    };
+    if(rightWristY>100 && rightWristY<=200){
+        song.rate(1);
+        document.getElementById("speed_val").innerHTML="1x";
+    }
+    if(rightWristY>200 && rightWristY<=300){
+        song.rate(1.5);
+        document.getElementById("speed_val").innerHTML="1.5x";
+    }
+    if(rightWristY>300 && rightWristY<=400){
+        song.rate(2);  
+        document.getElementById("speed_val").innerHTML="2x";
+    }
+    if(rightWristY>400 && rightWristY<=500){
+        song.rate(2.5); 
+        document.getElementById("speed_val").innerHTML="2.5x"; 
+    }
+}
+if(Rscore<0.2){
     document.getElementById("instruct").innerHTML="please keep a two feet distance and show your wrists to the camera";
 };
 };
